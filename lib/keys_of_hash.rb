255b9454d6759144bef1f3a9c6c8b88e45798ae9
Stephen Wright <scw248@gmail.com>
2019-01-11 18:17:03 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,11 +1,11 @@
 class Hash
   def keys_of(*arguments)
-    if arguments.include?(val.to_s)
     arguments.each do |species, country|
       species
     end
     elsif arguments.include?(val.to_i)
-    arguments.each do ||
+    arguments.each do |key, val|
+      
   end
   end
 end
